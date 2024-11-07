63e977a4d0b0fc4462d967f577033b3fcb70ff6c
snowricha <richa.sharma3@cognizant.com>
2024-11-07 18:22:23 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -18,7 +18,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
-st.stop()
+#st.stop()
 
 ingredients_list = st.multiselect('choose your top 5 ingredients:',my_dataframe,
                                  max_selections=5)
