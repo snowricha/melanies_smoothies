fe218c40d1875dc91a13028d60645a41973d847d
snowricha <richa.sharma3@cognizant.com>
2024-11-07 17:59:11 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -20,10 +20,11 @@ ingredients_list = st.multiselect('choose your top 5 ingredients:',my_dataframe,
 
 if ingredients_list: 
     ingredients_string = ''
-for fruit_chosen in ingredients_list:
-    ingredients_string += fruit_chosen+ ' '
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen+ ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_chosen)
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
 #st.write(ingredients_string)
 
