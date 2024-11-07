02c003fceaf5f4eadd0869f95d59161341d0bcda
snowricha <richa.sharma3@cognizant.com>
2024-11-07 17:56:05 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -22,7 +22,9 @@ if ingredients_list:
     ingredients_string = ''
 for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen+ ' '
-
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+    
 #st.write(ingredients_string)
 
 my_insert_stmt= """insert into smoothies.public.orders(ingredients,name_on_order)values ('"""+ingredients_string+"""','"""+Name_on_Order+"""')"""
@@ -34,6 +36,4 @@ if time_to_insert:
     st.success('Your Smoothie is ordered!',icon ="✅")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
