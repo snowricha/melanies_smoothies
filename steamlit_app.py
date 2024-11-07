d9d34929651ed8d2cf8109e582016f84bc1d5903
snowricha <richa.sharma3@cognizant.com>
2024-11-07 18:15:58 +0530

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -12,9 +12,9 @@ Name_on_Order = st.text_input('Name on Smoothie:')
 st.write('the name on the Smoothie will be ',Name_on_Order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True);
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+st.dataframe(data=my_dataframe, use_container_width=True);
+st.stop()
 ingredients_list = st.multiselect('choose your top 5 ingredients:',my_dataframe,
                                  max_selections=5)
 
